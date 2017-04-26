cbcc64cbf3f0b1233d1a58798f4414ee39e98d24
Philip A Senger <philip.a.senger@cngrgroup.com>
2017-04-26 23:57:09 +1000

    looking better

--- src/containers/userProfile/UserProfile.js
+++ src/containers/userProfile/UserProfile.js
@@ -3,9 +3,10 @@ import {connect} from "react-redux";
 import React, {Component} from "react";
 import {bindActionCreators} from "redux";
 import Form from "../../components/form";
+import InputText from '../../components/inputText';
 import {fetchProfile} from "../../services/api";
 import * as ProfileAction from "../../actionCreators/profileAction";
-import {Button,Alert,ProgressBar,FormGroup,ControlLabel,FormControl,HelpBlock} from "react-bootstrap";
+import {Button, ControlLabel, FormControl, FormGroup, HelpBlock, ProgressBar} from "react-bootstrap";
 
 const delayPromise = (ms) => {
     return (data) => {
@@ -23,22 +24,24 @@ export class UserProfile extends Component {
         super(props);
         this.displayName = 'containers/UserProfile';
         this.onSubmit = this.onSubmit.bind(this);
-        this.handleChange = this.handleChange.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.onValidate = this.onValidate.bind(this);
     }
 
     componentWillMount() {
+        /**
+         * Test to see if there is an id in the url, if so, load the record.
+         */
         if ( this.props.match && this.props.match.params && this.props.match.params.id ) this.load( this.props.match.params.id )
     }
 
-    load( id ) {
-        console.log( 'load called', id );
-        this.setState({loading:true},
-            ()=> {
+    load(id) {
+        this.setState({loading: true},
+            () => {
                 fetchProfile(id)
                     .then(delayPromise(1000))
                     .then((data) => {
-                        this.setState(data);
-                        // return this.props.profileReducer. // .updateProfiles(data);
+                        return this.props.profileActionDispatcher.updateProfile(data);
                     })
                     .then(() => {
                         this.setState({loading: false})
@@ -49,47 +52,85 @@ export class UserProfile extends Component {
     }
 
     onSubmit() {
+        // @todo PAS working on validation here.
+        // if ( onValidate('*') !== 'success' )
         // console.log('onSubmit', this.props.profileAction.updateEmail('xxxxx') );
         console.log('onSubmit');
        // this.setState({ type: 'info', message: 'Sending...' }, this.sendFormData);
     }
 
-    handleChange(fieldName) {
-        return (e) => {
-            console.log(e);
-            this.props.profileActionDispatcher.updateFirstName('larry')
-        }
+    onChange(fn) {
+       return function(e){
+           fn(e.target.value);
+       }
     }
-    getValidationState(){
-        console.log('getValidationState');
+
+    // _validator_ fires every time the render is called on InputText...
+    //    Which only happens when props are changed.
+    onValidate(name){
         // valid values are ["success","warning","error",null]
-        return null;
+         
+        return function() {
+            if ( name === 'name.first') {
+                if ( this.props.name.first !== '' && this.props.name.first.length <= 20 ) return 'success';
+                return 'error';
+            }
+            if ( name === 'name.last') {
+                if ( this.props.name.last !== '' && this.props.name.last.length <= 20 ) return 'success';
+                return 'error';
+            }
+            if ( name === 'email') {
+                var re = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+                if ( this.props.email !== '' && re.test(this.props.email) ) return 'success';
+                return 'error';
+            }
+            return 'success';
+        }.bind(this);
     }
 
     render() {
         return (
-            <Form>
-                {(this.state.loading) ?
-                    (
-                        <ProgressBar now={10} label="10" srOnly/>
-                    ):(
-                        <div>
-                            <FormGroup controlId="formBasicText"
-                                       validationState={this.getValidationState()}>
-                                <ControlLabel>First Name</ControlLabel>
-                                <FormControl type="text"
-                                             value={this.props.profileReducer.name.first}
-                                             placeholder="Enter first name"
-                                             onChange={this.handleChange('name.first')} />
-                                <FormControl.Feedback />
-                                <HelpBlock>Enter the first name</HelpBlock>
-                            </FormGroup>
-
-                            <Button type="button" className="btn btn-primary" onClick={this.onSubmit}>Submit</Button>
-                        </div>
-                    )
-                }
-            </Form>
+            <section>
+                <h1>Profile Edit</h1>
+                <Form>
+                    {(this.state.loading) ?
+                        (
+                            <ProgressBar now={10} label="10" srOnly/>
+                        ):(
+                            <div>
+                                <InputText
+                                    fieldId="firstName"
+                                    label="Enter the first name"
+                                    help="The first name is required and can be no larger than 20 characters"
+                                    placeholder="First Name"
+                                    value={this.props.name.first}
+                                    onChange={this.onChange(this.props.profileActionDispatcher.updateFirstName)}
+                                    validator={this.onValidate('name.first')}
+                                />
+                                <InputText
+                                    fieldId="lastName"
+                                    label="Enter the last name"
+                                    help="The last name is required and can be no larger than 20 characters"
+                                    placeholder="Last Name"
+                                    value={this.props.name.last}
+                                    onChange={this.onChange(this.props.profileActionDispatcher.updateLastName)}
+                                    validator={this.onValidate('name.last')}
+                                />
+                                <InputText
+                                    fieldId="email"
+                                    label="Enter the email"
+                                    help="The email is required and must be a valid format"
+                                    placeholder="Email"
+                                    value={this.props.email}
+                                    onChange={this.onChange(this.props.profileActionDispatcher.updateEmail)}
+                                    validator={this.onValidate('email')}
+                                />
+                                <Button type="button" className="btn btn-primary" onClick={this.onSubmit}>Submit</Button>
+                            </div>
+                        )
+                    }
+                </Form>
+            </section>
         );
     }
 }
@@ -99,64 +140,18 @@ UserProfile.propTypes = {
     profileReducer: PropTypes.object.isRequired
 };
 
-let mapStateToProps = (store /** , ownProps **/) => {
+let mapStateToProps = (state /** , ownProps **/) => {
     return {
-        profileReducer: store.profileReducer
+        profileReducer: state.profileReducer,
+        name: state.profileReducer.name,
+        email: state.profileReducer.email
     };
-}
+};
 
 let mapDispatchToProps = (dispatch) => {
     return {
         profileActionDispatcher: bindActionCreators(ProfileAction, dispatch)
     };
-}
-
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(UserProfile);
-
-/**
-    <MultiFormGroup id="name"
-                    label="Full Name">
-        <div>
-            <Input id="first"
-                   type="text"
-                   className="form-control"
-                   aria-describedby="firstHelp"
-                   placeholder="Enter first name"
-                   tabIndex="0"
-                   onChange={(e) => {
-                       this.props.profileAction.updateFirstName(e.target.value);
-                   }}/>
-            <small id="firstHelp" className="form-text text-muted">Enter your first name
-            </small>
-            <Input id="last"
-                   type="text"
-                   className="form-control"
-                   aria-describedby="lastHelp"
-                   placeholder="Enter email"
-                   tabIndex="0"
-                   onChange={(e) => {
-                       this.props.profileAction.updateLastName(e.target.value);
-                   }}/>
-            <small id="lastHelp"
-                   className="form-text text-muted">Enter your last name.
-            </small>
-        </div>
-    </MultiFormGroup>
-
-    <FormGroup inputId="email"
-        label="Email address">
-            <Input id="email"
-        type="email"
-        className="form-control"
-        aria-describedby="emailHelp"
-        placeholder="Enter email"
-        tabIndex="0"
-        onChange={(e) => {
-            this.props.profileAction.updateEmail(e.target.value);
-        }}/>
-        <small id="emailHelp"
-        className="form-text text-muted">Enter your email.
-        </small>
-    </FormGroup>
-**/
