c5b14e5e1b0b661267ab9fadee52a582e696c73c
Philip A Senger <philip.a.senger@cngrgroup.com>
2017-05-17 18:58:57 +1000

    Updated the actions

--- src/actionCreators/actionTypes/index.js
+++ src/actionCreators/actionTypes/index.js
@@ -1,3 +1,28 @@
-/**
- * Created by psenger on 5/17/17.
- */
+const ACTIONS = {
+    PROFILE: {
+      REQUEST: {
+        INITIATE: 'profile.request.initiate',
+        LOAD: 'profile.request.load',
+        SUCCESS: 'profile.request.success',
+        FAIL: 'profile.request.fail'
+      },
+      MODIFY: {
+        ACTIVE: 'profile.modify.active',
+        EMAIL: 'profile.modify.email',
+        FIRST_NAME: 'profile.modify.name.first',
+        LAST_NAME: 'profile.modify.name.last',
+        DATE_OF_BIRTH: 'profile.modify.dob'
+      }
+    },
+    PROFILES : {
+      REQUEST: {
+        INITIATE: 'profile.request.initiate',
+        LOAD: 'profile.request.load',
+        SUCCESS: 'profile.request.success',
+        FAIL: 'profile.request.fail'
+      },
+    }
+  };
+
+export {ACTIONS as default};
+
